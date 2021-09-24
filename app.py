f415239214867fd6586f60d7d4e37a2d9954d017
Little Ji <332420802@qq.com>
2021-09-24 14:23:10 +0800

    init

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from flask import Flask, url_for
 from flask import escape
 
@@ -16,12 +17,12 @@ def user_page(name):
 
 @app.route('/test')
 def test_url_for():
-    # ?????????????????????? URL??
-    print(url_for('hello'))  # ???/
-    # ??????????????? URL ??? URL ?
-    print(url_for('user_page', name='greyli'))  # ???/user/greyli
-    print(url_for('user_page', name='peter'))  # ???/user/peter
-    print(url_for('test_url_for'))  # ???/test
-    # ???????????????????????????????? URL ???
-    print(url_for('test_url_for', num=2))  # ???/test?num=2
+    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='greyli'))  # 输出：/user/greyli
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page123'
